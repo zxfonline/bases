f5773ba52209bf0c5b9853092fb634a5e93ed3a1
zxfonline <zxfonline@sina.com>
2017-05-15 19:41:15 +0800

    Benchmark
    add Benchmark test func

--- bases_test.go
+++ bases_test.go
@@ -25,6 +25,89 @@ type checkstruct struct {
 	exp   string
 }
 
+// the test data below is a map from base to lists of [input, exp]:
+var DATA = map[int][]checkstruct{
+	2: []checkstruct{
+		checkstruct{1, "1"},
+		checkstruct{2, "10"},
+		checkstruct{3, "11"},
+		checkstruct{4, "100"},
+		checkstruct{20, "10100"},
+	},
+	16: []checkstruct{
+		checkstruct{1, "1"},
+		checkstruct{2, "2"},
+		checkstruct{9, "9"},
+		checkstruct{10, "a"},
+		checkstruct{11, "b"},
+		checkstruct{15, "f"},
+		checkstruct{16, "10"},
+		checkstruct{17, "11"},
+		checkstruct{31, "1f"},
+		checkstruct{32, "20"},
+		checkstruct{33, "21"},
+		checkstruct{255, "ff"},
+		checkstruct{256, "100"},
+		checkstruct{257, "101"},
+	},
+	36: []checkstruct{
+		checkstruct{1, "1"},
+		checkstruct{9, "9"},
+		checkstruct{10, "a"},
+		checkstruct{35, "z"},
+		checkstruct{36, "10"},
+		checkstruct{37, "11"},
+		checkstruct{71, "1z"},
+		checkstruct{72, "20"},
+		checkstruct{73, "21"},
+		checkstruct{1295, "zz"},
+		checkstruct{1296, "100"},
+		checkstruct{1297, "101"},
+	},
+	62: []checkstruct{
+		checkstruct{1, "1"},
+		checkstruct{9, "9"},
+		checkstruct{10, "a"},
+		checkstruct{35, "z"},
+		checkstruct{36, "A"},
+		checkstruct{61, "Z"},
+		checkstruct{62, "10"},
+		checkstruct{63, "11"},
+		checkstruct{123, "1Z"},
+		checkstruct{124, "20"},
+		checkstruct{125, "21"},
+		checkstruct{3843, "ZZ"},
+		checkstruct{3844, "100"},
+		checkstruct{3845, "101"},
+	},
+	26: []checkstruct{
+		checkstruct{0, "a"},
+		checkstruct{1, "b"},
+		checkstruct{25, "z"},
+		checkstruct{26, "ba"},
+		checkstruct{27, "bb"},
+		checkstruct{51, "bz"},
+		checkstruct{52, "ca"},
+		checkstruct{53, "cb"},
+		checkstruct{675, "zz"},
+		checkstruct{676, "baa"},
+		checkstruct{677, "bab"},
+	},
+	52: []checkstruct{
+		checkstruct{0, "a"},
+		checkstruct{25, "z"},
+		checkstruct{26, "A"},
+		checkstruct{51, "Z"},
+		checkstruct{52, "ba"},
+		checkstruct{53, "bb"},
+		checkstruct{103, "bZ"},
+		checkstruct{104, "ca"},
+		checkstruct{105, "cb"},
+		checkstruct{2703, "ZZ"},
+		checkstruct{2704, "baa"},
+		checkstruct{2705, "bab"},
+	}}
+
 func TestConstFolduint64add(t *testing.T) {
 	// test zero across those bases:
 	for _, base := range BASES_BEGINNING_WITH_ZERO {
@@ -39,89 +122,6 @@ func TestConstFolduint64add(t *testing.T) {
 		assert.Equal(t, strconv.Itoa(num), ToBase(num, 10))
 	}
 
-	// the test data below is a map from base to lists of [input, exp]:
-	DATA := map[int][]checkstruct{
-		2: []checkstruct{
-			checkstruct{1, "1"},
-			checkstruct{2, "10"},
-			checkstruct{3, "11"},
-			checkstruct{4, "100"},
-			checkstruct{20, "10100"},
-		},
-		16: []checkstruct{
-			checkstruct{1, "1"},
-			checkstruct{2, "2"},
-			checkstruct{9, "9"},
-			checkstruct{10, "a"},
-			checkstruct{11, "b"},
-			checkstruct{15, "f"},
-			checkstruct{16, "10"},
-			checkstruct{17, "11"},
-			checkstruct{31, "1f"},
-			checkstruct{32, "20"},
-			checkstruct{33, "21"},
-			checkstruct{255, "ff"},
-			checkstruct{256, "100"},
-			checkstruct{257, "101"},
-		},
-		36: []checkstruct{
-			checkstruct{1, "1"},
-			checkstruct{9, "9"},
-			checkstruct{10, "a"},
-			checkstruct{35, "z"},
-			checkstruct{36, "10"},
-			checkstruct{37, "11"},
-			checkstruct{71, "1z"},
-			checkstruct{72, "20"},
-			checkstruct{73, "21"},
-			checkstruct{1295, "zz"},
-			checkstruct{1296, "100"},
-			checkstruct{1297, "101"},
-		},
-		62: []checkstruct{
-			checkstruct{1, "1"},
-			checkstruct{9, "9"},
-			checkstruct{10, "a"},
-			checkstruct{35, "z"},
-			checkstruct{36, "A"},
-			checkstruct{61, "Z"},
-			checkstruct{62, "10"},
-			checkstruct{63, "11"},
-			checkstruct{123, "1Z"},
-			checkstruct{124, "20"},
-			checkstruct{125, "21"},
-			checkstruct{3843, "ZZ"},
-			checkstruct{3844, "100"},
-			checkstruct{3845, "101"},
-		},
-		26: []checkstruct{
-			checkstruct{0, "a"},
-			checkstruct{1, "b"},
-			checkstruct{25, "z"},
-			checkstruct{26, "ba"},
-			checkstruct{27, "bb"},
-			checkstruct{51, "bz"},
-			checkstruct{52, "ca"},
-			checkstruct{53, "cb"},
-			checkstruct{675, "zz"},
-			checkstruct{676, "baa"},
-			checkstruct{677, "bab"},
-		},
-		52: []checkstruct{
-			checkstruct{0, "a"},
-			checkstruct{25, "z"},
-			checkstruct{26, "A"},
-			checkstruct{51, "Z"},
-			checkstruct{52, "ba"},
-			checkstruct{53, "bb"},
-			checkstruct{103, "bZ"},
-			checkstruct{104, "ca"},
-			checkstruct{105, "cb"},
-			checkstruct{2703, "ZZ"},
-			checkstruct{2704, "baa"},
-			checkstruct{2705, "bab"},
-		}}
-
 	// go through and test all of the above data:
 	for base, data := range DATA {
 		for i := 0; i < len(data); i++ {
@@ -132,4 +132,20 @@ func TestConstFolduint64add(t *testing.T) {
 		}
 	}
 
+}
+func Benchmark_ToBase(b *testing.B) {
+	for d := 0; d < b.N; d++ {
+		for base, data := range DATA {
+			for i := 0; i < len(data); i++ {
+				num := data[i].input
+				exp := data[i].exp
+				if exp != ToBase(num, base) {
+					b.Fatal("exp:%v,act:%v", exp, ToBase(num, base))
+				}
+				if num != FromBase(exp, base) {
+					b.Fatal("exp:%v,act:%v", num, FromBase(exp, base))
+				}
+			}
+		}
+	}
 }
